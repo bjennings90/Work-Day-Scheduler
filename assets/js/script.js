942ab7f6ab72afe348e9e03bccdd615873d0fa9c
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-06-25 11:00:05 -0500

    added rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-var time = moment(date, "L").set("hour", 17);
+var body = document.body;
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+// today = mm + '/' + dd + '/' + yyyy;
+// document.write(today);
+// date.setAttrbute('style', 'align-center');
+// body.appendChild(currentDay);
