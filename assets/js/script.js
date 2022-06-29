d0cbb7d7b5840bbbe0d2826d82e9b41903c72f25
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-06-29 10:29:23 -0500

    figuring out color coding for time boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 var textArea = $(".textarea");
-var hour = $(".hour");
+var hours = $(".hours");
+
+
 
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 var now = moment().format("H");
 console.log(now);
 
-var onePM = parseInt($("#onePM").text())+12;
-var twoPM = parseInt($("#twoPM").text())+12;
-var threePM = parseInt($("#threePM").text())+12;
-var fourPM = parseInt($("#fourPM").text())+12;
-var fivePM = parseInt($("#fivePM").text())+12;
+var onePM = parseInt($("#onePM").text()) + 12;
+var twoPM = parseInt($("#twoPM").text()) + 12;
+var threePM = parseInt($("#threePM").text()) + 12;
+var fourPM = parseInt($("#fourPM").text()) + 12;
+var fivePM = parseInt($("#fivePM").text()) + 12;
 
 var nineAM = parseInt($("#nineAM").text());
 var tenAM = parseInt($("#tenAM").text());
@@ -21,29 +23,29 @@ var twelvePM = parseInt($("#twelvePM").text());
 //     elevenAM.addClass("present");
 // }
 
-$(document).ready(function() {
+$(document).ready(function () {
     // listen for save button clicks
-    $('.saveBtn').on('click', function() {
-      // get nearby values
-      var value = $(this)
-        .siblings('.description')
-        .val();
-      var time = $(this)
-        .siblings()
-        .attr('id');
-  
+    $('.saveBtn').on('click', function () {
+        // get nearby values
+        var value = $(this)
+            .siblings('.description')
+            .val();
+        var time = $(this)
+            .siblings()
+            .attr('id');
+
         console.log(time, value)
-  
-      // save in localStorage
-      localStorage.setItem(time, value);
-  
-      // Show notification that item was saved to localStorage by adding class 'show'
-      $('.notification').addClass('show');
-  
-      // Timeout to remove 'show' class after 5 seconds
-      setTimeout(function() {
-        $('.notification').removeClass('show');
-      }, 5000);
+
+        // save in localStorage
+        localStorage.setItem(time, value);
+
+        // Show notification that item was saved to localStorage by adding class 'show'
+        $('.notification').addClass('show');
+
+        // Timeout to remove 'show' class after 5 seconds
+        setTimeout(function () {
+            $('.notification').removeClass('show');
+        }, 5000);
     });
 });
 
@@ -51,9 +53,23 @@ function timeTracker() {
     var curHour = moment().hours();
     if (curHour) {
         console.log(curHour)
-    $(".description").removeClass(".present").addClass(".past");
-    } else {
-    $(".description").removeClass(".present").addClass(".future");
+        $(".description").each(function (timeTracker) {
+            if ($("textArea").removeClass(".present").addClass(".past"));
+            if ($("textArea").removeClass(".present").addClass(".future"));
+
+        });
+    }
 };
-}
 console.log(timeTracker())
+
+
+// $(".description").each(function() {
+//     console.log(index + ":" + $(this).text());
+//     $("textArea").removeClass(".present").addClass(".past");
+// });
+
+// $(".description").each(function(index) {
+//     console.log(index + ":" + $(this).text());
+//     $("textArea").removeClass(".present").addClass(".future");
+// });
+
