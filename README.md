7d3dd8999017368f637647d20ee24527e1813989
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-06-29 22:44:05 -0500

    fill out readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Work Day Scheduler Starter Code
+This applicatoin shows work hours that schedules a persons to do list through out the workday. Color coding corrisponds with the current time, past time, and future time.
+
+![](assets/images/screencapture-Work-Day-Scheduler.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,8 @@ var currentTime = $("#currentDay");
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 var now = moment().format("H");
-console.log(now);
+
+
 
 var onePM = parseInt($("#onePM").text()) + 12;
 var twoPM = parseInt($("#twoPM").text()) + 12;
@@ -17,8 +18,10 @@ var fivePM = parseInt($("#fivePM").text()) + 12;
 
 var nineAM = parseInt($("#nineAM").text());
 var tenAM = parseInt($("#tenAM").text());
-var elevenAM = parseInt($("#eleventhAM").text());
+var elevenAM = parseInt($("#elevenAM").text());
 var twelvePM = parseInt($("#twelvePM").text());
+var timeArrayTextArea = [1, 2, 3, 4, 5, 9, 10, 11, 12];
+var timeArray = [onePM, twoPM, threePM, fourPM, fivePM, nineAM, tenAM, elevenAM, twelvePM];
 
 // if (elevenAM === now) {
 //     elevenAM.addClass("present");
@@ -35,7 +38,7 @@ $(document).ready(function () {
             .siblings()
             .attr('id');
 
-        console.log(time, value)
+        
 
         // save in localStorage
         localStorage.setItem(time, value);
@@ -50,45 +53,27 @@ $(document).ready(function () {
     });
 });
 
-// function timeTracker() {
-//     var curHour = moment().hours(); { 
-        
-//         if (currentTime > aSpecificTimeBlock) {
-//         aSpecificTimeBlock.className = ".past";
-//      } if else { (currentTime < aSpecificTimeBlock); {
-//         aSpecificTimeBlock.className = ".present";
-//      } else {
-//         aSpecificTimeBlock.className = ".future";
-//      };
-//     };
-// };
-// };
 function timeTracker() {
     var curHour = moment().hours();
-    console.log(curHour);
-    console.log(onePM);
-    console.log($(".description").each());
-    if (curHour) {
-        
-        $(".description").each(function (timeTracker) {
-            if ($("textArea").removeClass(".present").addClass(".past"));
-            if ($("textArea").removeClass(".present").addClass(".future"));
-
-        });
-    }
+    for (var i = 0; i < timeArray.length; i++) {
+        if (curHour === timeArray[i]) {
+            var timeArrayVal = timeArrayTextArea[i];
+            timeArrayVal = "#" + timeArrayVal;
+            $(timeArrayVal).addClass("present");
+        } 
+        if (curHour > timeArray[i]) {
+            var timeArrayVal = timeArrayTextArea[i];
+            timeArrayVal = "#" + timeArrayVal;
+            $(timeArrayVal).addClass("past");
+        }
+        if (curHour < timeArray[i]) {
+            var timeArrayVal = timeArrayTextArea[i];
+            timeArrayVal = "#" + timeArrayVal;
+            $(timeArrayVal).addClass("future");
+}
 };
+}
 
+$(".description").each(timeTracker);
 timeTracker();
-
-
-
-// $(".description").each(function() {
-//     console.log(index + ":" + $(this).text());
-//     $("textArea").removeClass(".present").addClass(".past");
-// });
-
-// $(".description").each(function(index) {
-//     console.log(index + ":" + $(this).text());
-//     $("textArea").removeClass(".present").addClass(".future");
-// });
 
